3205acfc70926f7c02797fe286fa2f64dd6e28f6
Fajer11 <fmalqarni@stu.kau.edu.sa>
2023-05-19 21:42:43 +0300

    Fajer Commit!

--- src/cpit251/b10/CPIT251B10.java
+++ src/cpit251/b10/CPIT251B10.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cpit251.b10;
 
-/**
- *
- * @author xesra
- */
+package cpit251.b10;
+import java.util.Scanner; 
 public class CPIT251B10 {
 
     /**
@@ -16,12 +8,49 @@ public class CPIT251B10 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello again 251>>");
-        System.out.println("Hi, raghad is back");
-        System.out.println("welcome");
-        System.out.println("We can do it, Fighting!");
-        System.out.println("Hello, This is Rama!");
-        System.out.println("HIIIII");
+        //System.out.println("Hello again 251>>");
+        //System.out.println("Hi, raghad is back");
+        //System.out.println("welcome");
+        //System.out.println("We can do it, Fighting!");
+        //System.out.println("Hello, This is Rama!");
+        //System.out.println("HIIIII");
+        Scanner input = new Scanner(System.in); 
+        UserInfo A = new UserInfo();
+        int userAnswer;
+        do{                     
+       System.out.println("<<Welcome to Teach Fit application>>");
+       System.out.println("Please answer the following questions to help us provide you with better services");
+       System.out.println("If you are enter Click 1, If you are not enter Click 2");
+       userAnswer = input.nextInt();
+       }
+  while(userAnswer != 1 || userAnswer != 2);
+        
+        if (userAnswer ==1) {
+  
+                System.out.println("What is your name?");
+                String name = input.nextLine();
+                A.setUserName(name);
+                System.out.println("How old are you?");
+                int Age = input.nextInt();
+                A.setUserAge(Age);
+                System.out.println("Your gender?");
+                String Gender = input.nextLine();
+                A.setUserGender(Gender);
+                System.out.println("How much do you weigh?");
+                int Weight = input.nextInt();
+                A.setUserWeight(Weight);
+                System.out.println("How much your height?");
+                int Height = input.nextInt();
+                A.setUserHeight(Height);
+                System.out.println("Express your activity from 0 to 3");
+                int Active = input.nextInt();
+                A.setUserActive(Active);
+                System.out.println("");
+            
+            if (userAnswer == 2)
+                {
+                System.out.println("Witing for you next time(:");
+                        }
     }
-    
-}
+    }
+    }
