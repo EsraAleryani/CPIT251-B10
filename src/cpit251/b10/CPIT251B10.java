1f1d0e1b4ab233799d3cce3da3e968e6321fcf4e
sraaOthman <snomanothman@stu.kau.edu.sa>
2023-05-25 18:40:35 +0300

    sraa commit

--- src/cpit251/b10/CPIT251B10.java
+++ src/cpit251/b10/CPIT251B10.java
@@ -16,44 +16,41 @@ public class CPIT251B10 {
         Scanner input = new Scanner(System.in); 
         
         UserInfo A = new UserInfo();
-        Employee employee = new Employee();
-        ArrayList<Employee> employeeList = new ArrayList<> ();
+        
+        Employee [] employee = new Employee [4];
+        ourEmp(employee);
+        
         Calories calories = null;
         Calories UserCalories = new Calories() ;
-        employee = new Employee("Ahmed Al-Ghamdi","Nutrition Specialist", 3.5, 599, "Nutritionist Specialized in Pediatric Dietian and Nutrition, Adult Internal Medicine","Male");
-        employeeList.add(employee);
-        employee = new Employee("Sarah Osama","Nutrition Specialist", 4.3, 650, "Nutritionist Specialized in Pediatric Dietian and Nutrition, Adult Dietitain","Fmale");
-        employeeList.add(employee);
-        employee = new Employee("Maryam Azhari","Nutrition Specialist", 5 , 300, "Nutritionist Specialized in  Adult Dietitian and Nutrition , Pediatric Dietian ","Fmale");
-        employeeList.add(employee);
+        
+        
         int userAnswer;
-        do{                     
+        //do{                     
        System.out.println("<<Welcome to Teach Fit application>>");
        System.out.println("Please answer the following questions to help us provide you with better services");
        System.out.println("If you are enter Click 1, If you are not enter Click 2");
        userAnswer = input.nextInt();
-       }
-        while(userAnswer != 1 || userAnswer != 2);
+       //}
+       // while(userAnswer != 1 || userAnswer != 2);
             
         if (userAnswer ==1) {
   
                 System.out.println("What is your name?");
-                String name = input.nextLine();        
+                String name = input.next();        
                 System.out.println("How old are you?");
                 int Age = input.nextInt();           
                 System.out.println("Your gender?");
-                String Gender = input.nextLine();         
+                String Gender = input.next();         
                 System.out.println("How much do you weigh?");
                 int Weight = input.nextInt();               
                 System.out.println("How much your height?");
-                double Height = input.nextInt();
+                double Height = input.nextDouble();
                 System.out.println("Express your activity from 0 to 3");
                 int Active = input.nextInt();
                  
             A = new UserInfo(name , Age , Gender , Weight , Height , Active);
             System.out.println("");
-            
-            
+   
             UserCalories = new Calories (name, Age, Gender, Height, Weight, Active, calories);
             System.out.printf("Your daily calorie needs are: %.0f calories", UserCalories.calories);
             
@@ -62,5 +59,12 @@ public class CPIT251B10 {
                 System.out.println("Witing for you next time(:");
                         }
     }
+    }
+    public static void ourEmp ( Employee [] employee){
+        employee[0] = new Employee(1,"Ahmed Al-Ghamdi","Nutrition Specialist", 3.5, 599, "Nutritionist Specialized in Pediatric Dietian and Nutrition, Adult Internal Medicine","Male");
+        employee [1] = new Employee(2,"Sarah Osama","Nutrition Specialist", 4.3, 650, "Nutritionist Specialized in Pediatric Dietian and Nutrition, Adult Dietitain","Fmale");
+        employee[2] = new Employee(3,"Hala saad ","Coach", 5.0, 200, "Specialized in gain weight","Male");
+        employee [3] = new Employee(4,"Maryam Gamil","Coach", 4.5, 399, "Specialized in loss weight","Fmale");
+          
     }
     }
