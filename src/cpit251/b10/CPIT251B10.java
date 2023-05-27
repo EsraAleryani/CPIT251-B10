564d9f451ae10545add6fbf8578ebcc28ad9e8ee
Esra <xesra2001x@gmail.com>
2023-05-28 01:14:19 +0300

    esra commit

--- src/cpit251/b10/CPIT251B10.java
+++ src/cpit251/b10/CPIT251B10.java
@@ -1,11 +1,14 @@
 
 package cpit251.b10;
  
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
 public class CPIT251B10 {
-
+static Scanner inputInt = new Scanner(System.in); 
+static  Calories UserCalories;
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         // TODO code application logic here
         //System.out.println("Hello again 251>>");
         //System.out.println("Hi, raghad is back");
@@ -13,28 +16,29 @@ public class CPIT251B10 {
         //System.out.println("We can do it, Fighting!");
         //System.out.println("Hello, This is Rama!");
         //System.out.println("HIIIII");
-        Scanner inputInt = new Scanner(System.in); 
+        
         Scanner inputStr = new Scanner(System.in); 
 
         UserInfo A = new UserInfo();
         
         Employee [] employee = new Employee [4];
-        ourEmp(employee);
+       ourEmp(employee);
         
         double calories = 0;
-        Calories UserCalories = new Calories() ;
+         UserCalories = new Calories() ;
         
         
        int userAnswer;
-       do{                     
+       
+     //  do{                     
        System.out.println("<<Welcome to Teach Fit application>>");
        System.out.println("Please answer the following questions to help us provide you with better services");
        System.out.println("If you are enter Click 1, If you are not enter Click 2");
        userAnswer = inputStr.nextInt();
-       }
-       while(userAnswer != 1 || userAnswer != 2);
+   //    }
+      // while(userAnswer != 1 || userAnswer != 2);
        
-            
+          
         if (userAnswer ==1) {
   
                 System.out.println("What is your name?");
@@ -55,7 +59,10 @@ public class CPIT251B10 {
    
             UserCalories = new Calories (name, Age, Gender, Height, Weight, Active, calories);
             System.out.printf("Your daily calorie needs are: %.0f calories", UserCalories.calCalories());
+            double c = UserCalories.calCalories();
             
+      EmpMenu( employee );
+            plan(employee,c);
             if (userAnswer == 2)
                 {
                 System.out.println("Witing for you next time(:");
@@ -63,10 +70,38 @@ public class CPIT251B10 {
     }
     }
     public static void ourEmp ( Employee [] employee){
-        employee[0] = new Employee(1,"Ahmed Al-Ghamdi","Nutrition Specialist", 3.5, 599, "Nutritionist Specialized in Pediatric Dietian and Nutrition, Adult Internal Medicine","Male");
-        employee [1] = new Employee(2,"Sarah Osama","Nutrition Specialist", 4.3, 650, "Nutritionist Specialized in Pediatric Dietian and Nutrition, Adult Dietitain","Fmale");
+        employee[0] = new Employee(1,"Ahmed Al-Ghamdi","Nutrition Specialist", 3.5, 599, " Specialized in Adult Internal Medicine","Male");
+        employee [1] = new Employee(2,"Sarah Osama","Nutrition Specialist", 4.3, 650, " Specialized in Adult Dietitain","Female");
         employee[2] = new Employee(3,"Hala saad ","Coach", 5.0, 200, "Specialized in gain weight","Male");
-        employee [3] = new Employee(4,"Maryam Gamil","Coach", 4.5, 399, "Specialized in loss weight","Fmale");
+        employee [3] = new Employee(4,"Maryam Gamil","Coach", 4.5, 399, "Specialized in loss weight","Female");
           
+    }
+    public static void EmpMenu(Employee [] employee ) {
+        System.out.println("Employ information:  ");                                                                                           
+        System.out.println("ID           Name                       Role              Rate     Fees                                    Career             Gender   ");
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------");
+        for(int i = 0 ; i < employee.length ; i++){
+            System.out.printf("%d %20s %29s %8.1f %10.1f %47s %11s",employee[i].empNum,employee[i].eName,employee[i].role,employee[i].rating,employee[i].fees,employee[i].CareerD,employee[i].gender);
+            System.out.println();
+        }
+        
+    }
+    
+    public static void plan(Employee [] employee , double c) throws FileNotFoundException, IOException{
+        System.out.println("Please select the employee you want work with by his id: ");
+        int choice= inputInt.nextInt();
+        if(choice == 1){
+           employee[1].generatePlan(employee[1],c);
+        }
+        else if(choice == 2){
+             employee[2] .generatePlan(employee[2],c);
+        }
+        else if(choice == 3){
+             employee[3] .generatePlan(employee[3],c);
+        }
+        else{
+             employee[4] .generatePlan(employee[4],c);
+        }
+        
     }
     }
