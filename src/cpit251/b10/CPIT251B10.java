1f49d5674fee65a0789ada12be78f4422f5a131c
Ramah Almufarijji <ralmufrrji0001@stu.kau.edu.sa>
2023-05-28 20:19:55 +0300

    Ramah Commit

--- src/cpit251/b10/CPIT251B10.java
+++ src/cpit251/b10/CPIT251B10.java
@@ -1,13 +1,15 @@
 
 package cpit251.b10;
- 
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
+
 public class CPIT251B10 {
-static Scanner inputInt = new Scanner(System.in); 
-static  Calories UserCalories;
-    
+
+    static Scanner inputInt = new Scanner(System.in);
+    static Calories UserCalories;
+
     public static void main(String[] args) throws FileNotFoundException, IOException {
         // TODO code application logic here
         //System.out.println("Hello again 251>>");
@@ -16,92 +18,110 @@ static  Calories UserCalories;
         //System.out.println("We can do it, Fighting!");
         //System.out.println("Hello, This is Rama!");
         //System.out.println("HIIIII");
-        
-        Scanner inputStr = new Scanner(System.in); 
+
+        Scanner inputStr = new Scanner(System.in);
 
         UserInfo A = new UserInfo();
-        
-        Employee [] employee = new Employee [4];
-       ourEmp(employee);
-        
+
+        Employee[] employee = new Employee[4];
+        ourEmp(employee);
+
         double calories = 0;
-         UserCalories = new Calories() ;
-        
-        
-       int userAnswer;
-       
-     //  do{                     
-       System.out.println("<<Welcome to Teach Fit application>>");
-       System.out.println("Please answer the following questions to help us provide you with better services");
-       System.out.println("If you are enter Click 1, If you are not enter Click 2");
-       userAnswer = inputStr.nextInt();
-   //    }
-      // while(userAnswer != 1 || userAnswer != 2);
-       
-          
-        if (userAnswer ==1) {
-  
+        UserCalories = new Calories();
+
+        int userAnswer;
+
+        do {
+            System.out.println("<<Welcome to Teach Fit application>>");
+            System.out.println("Please answer the following questions to help us provide you with better services");
+            System.out.print("If you are enter Click 1 \nIf you are not enter Click 2 \nIf exit click any  other key \nSelection: ");
+            userAnswer = inputStr.nextInt();
+
+            if (userAnswer == 1) {
+
                 System.out.println("What is your name?");
-                String name = inputStr.next();        
+                String name = inputStr.next();
                 System.out.println("How old are you?");
-                int Age = inputInt.nextInt();           
+                int Age = inputInt.nextInt();
                 System.out.println("Your gender Female(F), Male(M)?");
-                String Gender = inputStr.next();         
+                String Gender = inputStr.next();
                 System.out.println("How much do you weigh?");
-                double Weight = inputInt.nextInt();               
+                double Weight = inputInt.nextInt();
                 System.out.println("How much your height?");
                 double Height = inputInt.nextDouble();
                 System.out.println("Express your activity from 0 to 3");
-                int Active = inputInt.nextInt();
-                 
-            A = new UserInfo(name , Age , Gender , Weight , Height , Active);
-            System.out.println("");
-   
-            UserCalories = new Calories (name, Age, Gender, Height, Weight, Active, calories);
-            System.out.printf("Your daily calorie needs are: %.0f calories", UserCalories.calCalories());
-            double c = UserCalories.calCalories();
-            
-      EmpMenu( employee );
-            plan(employee,c);
-            if (userAnswer == 2)
-                {
+                int Active = inputInt.nextInt();              
+
+                A = new UserInfo(name, Age, Gender, Weight, Height, Active);
+                System.out.println("");
+
+                UserCalories = new Calories(name, Age, Gender, Height, Weight, Active, calories);
+                System.out.printf("Your daily calorie needs are: %.0f calories", UserCalories.calCalories());
+                double c = UserCalories.calCalories();
+
+                EmpMenu(employee);
+                plan(employee, c);
+
+                int servicenum = 0;
+                Payment payment = new Payment("not specified yet", "not specified yet", "not specified yet", 0);
+                while (servicenum != 5) {
+                    System.out.println("");
+                    System.out.print("select service: \n 1]Coach \n 2]Neutrition \n 3]Membership \n 4]Print Invoce \n 5]No Service \n Selection: ");
+                    servicenum = inputInt.nextInt();
+                    System.out.println("");
+                    if (servicenum == 1) {
+                        payment.Coach();
+                    } else if (servicenum == 2) {
+                        payment.Neutrition();
+                    } else if (servicenum == 3) {
+                        payment.Mmembership();
+                    } else if (servicenum == 4) {
+                        payment.displayPayment();
+                    } else {
+                        break;
+                    }
+                }
+
+            } else if (userAnswer == 2) {
                 System.out.println("Witing for you next time(:");
-                        }
+            }
+        } while (userAnswer == 1 || userAnswer == 2);
     }
+     
+    
+   
+    public static void ourEmp(Employee[] employee) {
+        employee[0] = new Employee(1, "Ahmed Al-Ghamdi", "Nutrition Specialist", 3.5, 599, " Specialized in Adult Internal Medicine", "Male");
+        employee[1] = new Employee(2, "Sarah Osama", "Nutrition Specialist", 4.3, 650, " Specialized in Adult Dietitain", "Female");
+        employee[2] = new Employee(3, "Hala saad ", "Coach", 5.0, 200, "Specialized in gain weight", "Male");
+        employee[3] = new Employee(4, "Maryam Gamil", "Coach", 4.5, 399, "Specialized in loss weight", "Female");
+
     }
-    public static void ourEmp ( Employee [] employee){
-        employee[0] = new Employee(1,"Ahmed Al-Ghamdi","Nutrition Specialist", 3.5, 599, " Specialized in Adult Internal Medicine","Male");
-        employee [1] = new Employee(2,"Sarah Osama","Nutrition Specialist", 4.3, 650, " Specialized in Adult Dietitain","Female");
-        employee[2] = new Employee(3,"Hala saad ","Coach", 5.0, 200, "Specialized in gain weight","Male");
-        employee [3] = new Employee(4,"Maryam Gamil","Coach", 4.5, 399, "Specialized in loss weight","Female");
-          
-    }
-    public static void EmpMenu(Employee [] employee ) {
-        System.out.println("Employ information:  ");                                                                                           
+
+    public static void EmpMenu(Employee[] employee) {
+        System.out.println("Employ information:  ");
         System.out.println("ID           Name                       Role              Rate     Fees                                    Career             Gender   ");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------");
-        for(int i = 0 ; i < employee.length ; i++){
-            System.out.printf("%d %20s %29s %8.1f %10.1f %47s %11s",employee[i].empNum,employee[i].eName,employee[i].role,employee[i].rating,employee[i].fees,employee[i].CareerD,employee[i].gender);
+        for (int i = 0; i < employee.length; i++) {
+            System.out.printf("%d %20s %29s %8.1f %10.1f %47s %11s", employee[i].empNum, employee[i].eName, employee[i].role, employee[i].rating, employee[i].fees, employee[i].CareerD, employee[i].gender);
             System.out.println();
         }
-        
+
     }
-    
-    public static void plan(Employee [] employee , double c) throws FileNotFoundException, IOException{
+
+    public static void plan(Employee[] employee, double c) throws FileNotFoundException, IOException {
         System.out.println("Please select the employee you want work with by his id: ");
-        int choice= inputInt.nextInt();
-        if(choice == 1){
-           employee[1].generatePlan(employee[1],c);
+        int choice = inputInt.nextInt();
+        if (choice == 1) {
+            employee[1].generatePlan(employee[1], c);
+        } else if (choice == 2) {
+            employee[2].generatePlan(employee[2], c);
+        } else if (choice == 3) {
+            employee[3].generatePlan(employee[3], c);
+        } else {
+            employee[4].generatePlan(employee[4], c);
         }
-        else if(choice == 2){
-             employee[2] .generatePlan(employee[2],c);
-        }
-        else if(choice == 3){
-             employee[3] .generatePlan(employee[3],c);
-        }
-        else{
-             employee[4] .generatePlan(employee[4],c);
-        }
-        
+
     }
-    }
+}
+
