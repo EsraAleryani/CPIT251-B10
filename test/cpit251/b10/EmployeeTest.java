b99e840a8955ea8e7c2b7fb712254f16d47d8258
sraaOthman <snomanothman@stu.kau.edu.sa>
2023-06-02 22:01:20 +0300

    sraa commit

---
+++ test/cpit251/b10/EmployeeTest.java
@@ -0,0 +1,290 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpit251.b10;
+
+import static cpit251.b10.CPIT251B10.employee;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author sraa
+ */
+public class EmployeeTest {
+    
+    public EmployeeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getArray method, of class Employee.
+     */
+    @Ignore
+    public void testGetArray() {
+        System.out.println("getArray");
+        Employee instance = new Employee();
+        Employee[] expResult = null;
+        Employee[] result = instance.getArray();
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setArray method, of class Employee.
+     */
+    @Ignore
+    public void testSetArray() {
+        System.out.println("setArray");
+        Employee[] array = null;
+        Employee instance = new Employee();
+        instance.setArray(array);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of geteName method, of class Employee.
+     */
+    @Ignore
+    public void testGeteName() {
+        System.out.println("geteName");
+        Employee instance = new Employee();
+        String expResult = "";
+        String result = instance.geteName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of seteName method, of class Employee.
+     */
+    @Ignore
+    public void testSeteName() {
+        System.out.println("seteName");
+        String eName = "";
+        Employee instance = new Employee();
+        instance.seteName(eName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getRole method, of class Employee.
+     */
+    @Ignore
+    public void testGetRole() {
+        System.out.println("getRole");
+        Employee instance = new Employee();
+        String expResult = "";
+        String result = instance.getRole();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setRole method, of class Employee.
+     */
+    @Ignore
+    public void testSetRole() {
+        System.out.println("setRole");
+        String role = "";
+        Employee instance = new Employee();
+        instance.setRole(role);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getRating method, of class Employee.
+     */
+    @Ignore
+    public void testGetRating() {
+        System.out.println("getRating");
+        Employee instance = new Employee();
+        double expResult = 0.0;
+        double result = instance.getRating();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setRating method, of class Employee.
+     */
+    @Ignore
+    public void testSetRating() {
+        System.out.println("setRating");
+        int rating = 0;
+        Employee instance = new Employee();
+        instance.setRating(rating);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getFees method, of class Employee.
+     */
+    @Ignore
+    public void testGetFees() {
+        System.out.println("getFees");
+        Employee instance = new Employee();
+        double expResult = 0.0;
+        double result = instance.getFees();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setFees method, of class Employee.
+     */
+    @Ignore
+    public void testSetFees() {
+        System.out.println("setFees");
+        int fees = 0;
+        Employee instance = new Employee();
+        instance.setFees(fees);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCareerD method, of class Employee.
+     */
+    @Ignore
+    public void testGetCareerD() {
+        System.out.println("getCareerD");
+        Employee instance = new Employee();
+        String expResult = "";
+        String result = instance.getCareerD();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCareerD method, of class Employee.
+     */
+    @Ignore
+    public void testSetCareerD() {
+        System.out.println("setCareerD");
+        String CareerD = "";
+        Employee instance = new Employee();
+        instance.setCareerD(CareerD);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getGender method, of class Employee.
+     */
+    @Ignore
+    public void testGetGender() {
+        System.out.println("getGender");
+        Employee instance = new Employee();
+        String expResult = "";
+        String result = instance.getGender();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setGender method, of class Employee.
+     */
+    @Ignore
+    public void testSetGender() {
+        System.out.println("setGender");
+        String gender = "";
+        Employee instance = new Employee();
+        instance.setGender(gender);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEmpNum method, of class Employee.
+     */
+    @Ignore
+    public void testGetEmpNum() {
+        System.out.println("getEmpNum");
+        Employee instance = new Employee();
+        int expResult = 0;
+        int result = instance.getEmpNum();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEmpNum method, of class Employee.
+     */
+    @Ignore
+    public void testSetEmpNum() {
+        System.out.println("setEmpNum");
+        int empNum = 0;
+        Employee instance = new Employee();
+        instance.setEmpNum(empNum);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of generatePlan method, of class Employee.
+     */
+    @Ignore
+    public void testGeneratePlan() throws Exception {
+        System.out.println("generatePlan");
+        Employee employee = null;
+        double UserCalories = 0.0;
+        Employee instance = new Employee();
+        instance.generatePlan(employee, UserCalories);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    
+    @Test (timeout = 1)
+    public void testOurEmp() {
+        System.out.println("ourEmp");
+        Employee a =new Employee();
+        Employee expectedResult = new Employee(4, "Maryam Gamil", "Coach", 4.5, 399, "Specialized in loss weight", "Female");
+        a.ourEmp(employee);
+        Employee [] result = employee;
+        assertEquals(expectedResult.geteName(), result[result.length-1].geteName());
+        
+    }
+    @Test
+    public void notNull(){
+        Employee a =new Employee();
+        a.ourEmp(employee);
+        assertNotNull(employee[1]);
+    }
+    
+}
