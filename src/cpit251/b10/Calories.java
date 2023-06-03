612eb12804b0ad90f25f030bdba9c5a7a37abf0b
Ramah Almufarijji <ralmufrrji0001@stu.kau.edu.sa>
2023-06-03 23:55:23 +0300

    Ramah Commit

--- src/cpit251/b10/Calories.java
+++ src/cpit251/b10/Calories.java
@@ -19,6 +19,7 @@ public class Calories {
 
     
     
+    
     public Calories(String Name, int age, String gender, double height, double Weight, int active, double calories) {
         this.Name = Name;
         this.age = age;
